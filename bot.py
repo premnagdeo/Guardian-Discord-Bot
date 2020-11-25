d7170e81a098bfdccc9281382563f5262a918884
premnagdeo <premnagdeo@gmail.com>
2020-11-25 17:41:01 +0530

    Added logging

--- bot.py
+++ bot.py
@@ -4,12 +4,20 @@ import discord
 from discord.ext import commands
 from collections import defaultdict
 from cogs._db_helper import Database
+import logging
 
-description = '''A bot to help you stay safe by creating timers that ping you to check whether you are safe or not.'''
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='bot.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+
+description = '''A bot to help you stay safe by creating timers that ping you to check whether you are safe or not. Your personal Guardian.'''
 intents = discord.Intents().all()
 
 bot = commands.Bot(command_prefix='!', description=description, intents=intents, case_insensitive=True)
-bot.timers = defaultdict(dict)
+
 bot.db = Database('bot.db')
 bot.version = "v0.1"
 
